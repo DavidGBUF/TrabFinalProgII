299b5fc9f77c5bd82b53d39b215a4e2c93264d70
DavidGBUF <129506782+DavidGBUF@users.noreply.github.com>
2025-03-07 22:31:47 -0300

    Refatoração David

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import models.Musica;
+import musicPlayer.Catalogo;
+import musicPlayer.Loader;
+
+public class Main {
+    public static void main(String[] args) {
+        String diretorioMusicas = "src/main/resources";
+        Catalogo catalogo = Loader.carregarCatalogo(diretorioMusicas);
+
+        // Exibe as músicas carregadas
+        for (Musica musica : catalogo.getMusicas()) {
+            System.out.println(musica.toString());
+        }
+
+
+
+    }
+}
